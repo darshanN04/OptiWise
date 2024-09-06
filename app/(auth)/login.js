1a4d09d39c41f359c13eeb461232a8cfb382480e
darshanN04 <darshannakshatri@gmail.com>
2024-09-07 00:20:12 +0530

    landing page

--- app/(auth)/login.js
+++ app/(auth)/login.js
@@ -1,12 +1,36 @@
 import { View, Text, StyleSheet } from 'react-native';
 import FormField from '../../components/FormField.js';
+import { useState } from 'react';
+import { Link } from 'expo-router';
+
+
+
 export default function Login() {
+  const [form, setForm]=useState({
+    email: "",
+    password: ""
+  })
   return (
     <View style={styles.container}>
       <Text style={styles.login}>Login</Text>
-      <FormField 
-        title="Email"
-        placeholder="Enter Email"/>
+        
+        <FormField 
+        placeholder="Enter Email"
+        value={form.email}
+        handleChangeText={(e) => setForm({...form, email: e })}/>
+
+        <FormField
+        placeholder="Enter Password"
+        value={form.password}
+        handleChangeText={(e) => setForm({...form, password: e })}/>
+      
+      <View style={styles.buttonContainer}>
+          <Link href="../(work)/patientReg" style={styles.button}>
+            <Text>Submit</Text>
+          </Link>
+          
+        </View>
+
     </View>
   );
 }
@@ -16,10 +40,27 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    paddingBottom: 50
   },
   login: {
     fontSize: 24,
-    marginBottom: 20,
+    marginBottom: 40,
     fontWeight: 'bold',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    width: '100%',
+  },
+  button: {
+    padding: 10,
+    backgroundColor: '#007bff',
+    color: 'white',
+    textAlign: 'center',
+    borderRadius: 5,
+    margin: 10,
+    width: 100,
+    marginTop: 50,
+    fontSize: 15
   },
 });
---
+++ constants/icons.js
@@ -0,0 +1,8 @@
+
+import eyeHide from "../assets/icons/eye-hide.png";
+import eye from "../assets/icons/eye.png";
+
+export default {
+  eyeHide,
+  eye
+};

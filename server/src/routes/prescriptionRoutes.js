28d1331e4ae21990a9e4db3c5167e23fcf30d691
darshanN04 <darshannakshatri@gmail.com>
2024-10-07 23:26:32 +0530

    darshan pulled server

--- server/src/routes/prescriptionRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getPrescriptionIds,getPrescriptionById,getPrescriptionDetails } from '../controllers/prescriptionController.js';
-const router = express.Router();
-
-router.get('/prescriptions/patient/:patient_id', getPrescriptionIds);
-router.get('/prescriptions/prescription/:id', getPrescriptionById);
-router.get('/prescription/:id', getPrescriptionDetails);
-
-export default router;
